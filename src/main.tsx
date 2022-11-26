56b08fcd39c9ccadc73aa3cfc20cc7323d4094bc
Luis Mera <luismera86@gmail.com>
2022-11-26 15:46:02 -0300

    Bases de redux toolkit agragadas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 
+import App from './App'
+import { Provider } from 'react-redux'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeConfig } from './config/theme.config'
+import { store } from './redux/app'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<ThemeConfig>
+				<App />
+			</ThemeConfig>
+		</Provider>
+	</React.StrictMode>
 )
